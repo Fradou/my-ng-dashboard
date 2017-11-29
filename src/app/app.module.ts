ca546972dd8c8afbcd7242e5c85e10cc0d88c974
Alexandre Fradet <alfradev@gmail.com>
2017-11-29 23:05:41 +0100

    Create Skill component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SkillComponent } from './skill/skill.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule
