1d56a97ad54f228289d033b4cbd05547d0e87a68
Alexandre Fradet <alfradev@gmail.com>
2017-12-03 11:06:46 +0100

    Add vault module that will be used for welcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppComponent } from './app.component';
 import { SkillsModule } from './skills/skills.module';
 import { SkillService } from './skills/shared/skill.service';
 import { AppRoutingModule } from './app-routing.module';
+import { VaultComponent } from './vault/vault.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    VaultComponent
   ],
   imports: [
     BrowserModule,
