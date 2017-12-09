1726c6c8a8e67ba7d2af5a670580197752796809
Alexandre Fradet <alfradev@gmail.com>
2017-12-09 08:00:21 +0100

    Moved custom-material in shared. Create first directive for hightlight.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [HighlightDirective]
 })
 export class CoreModule { }
